d947ce16788dcb5b1de92ddc5611c0647433f1af
kaka <1823188198@qq.com>
2026-05-31 00:00:02 +0800

    前端页面渲染

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "antd";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <Button type="primary" htmlType="submit">
+          primary Button
+        </Button>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
